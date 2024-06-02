95312fae1e0358725e9d9757c552cc1e9258bd86
José Cunha Teixeira <148117375+JoseCunhaTeixeira@users.noreply.github.com>
2024-06-02 20:58:00 +0200

    Add files via upload

--- main_SANTILUDO.py
+++ main_SANTILUDO.py
@@ -95,25 +95,6 @@ kw = 2.3e9 # Water bulk modulus [Pa]
 ka = 1.01e5 # Air bulk modulus [Pa]
 g = 9.82 # Gravity acceleration [m/s2]
 
-# Soil layers
-soiltypes = ['sand', 'clay'] # Soil types (see list in selectSoilType.m with associated VG parameters and mixture)
-thicknesses = [5, 5] # Layer thicknesses [m]
-
-# Grains/agregate parameters per layer
-Ns = [8, 8] # Coordination Number (number of contact per grain) | default = 8
-fracs = [0.3, 0.3] # Fraction of non-slipping grains (helps making the soil less stiff)
-
-# Geometry and discretisation of the medium
-depth = np.sum(thicknesses) # Depth of the soil column [m]
-dz = 0.01 # Depth sample interval [m]
-top_surface_level = dz # Altitude of the soil surface[m]
-zs = -np.arange(top_surface_level, depth + dz, dz) # Depth positions (negative downward) [m]
-NbCells = len(zs) - 1 # Number of exploration points in depth [#]
-
-# Water table
-WTs = [2.5, 7.5] # Water table depths [m]
-color_map = copper(np.linspace(0, 1, len(WTs))) # Colorscale for plots if several WT tested
-
 # Grains/agregate mechanical properties
 mu_clay = 6.8 # Shear moduli [GPa]
 mu_silt = 45.0
@@ -124,6 +105,25 @@ k_sand = 37.0
 rho_clay = 2580.0 # Density [kg/m3]
 rho_silt = 2600.0
 rho_sand = 2600.0
+
+# Soil layers
+soiltypes = ['sand', 'clay'] # Soil types (see list in selectSoilType.m with associated VG parameters and mixture)
+thicknesses = [5, 10] # Layer thicknesses [m]
+
+# Grains/agregate parameters per layer
+Ns = [8, 8] # Coordination Number (number of contact per grain) | default = 8
+fracs = [0.3, 0.3] # Fraction of non-slipping grains (helps making the soil less stiff) | default = 0.3
+
+# Water table
+WTs = [1, 2, 3, 4, 5] # Water table depths [m]
+color_map = copper(np.linspace(0, 1, len(WTs))) # Colorscale for plots if several WT tested
+
+# Geometry and discretisation of the medium
+depth = np.sum(thicknesses) # Depth of the soil column [m]
+dz = 0.01 # Depth sample interval [m]
+top_surface_level = dz # Altitude of the soil surface[m]
+zs = -np.arange(top_surface_level, depth + dz, dz) # Depth positions (negative downward) [m]
+NbCells = len(zs) - 1 # Number of exploration points in depth [#]
 
 # Three possible RP models:
 # kk = 1 # Constant Pe (see the approach of Zyserman et al., 2017)
@@ -138,7 +138,7 @@ kk = 3 # Pe with suction (cf. Solazzi et al. 2021)
 # In GPDC format : "thickness Vp Vs rho\n"
 # Each layer is separated by \n | Only spaces between values | Last layer thickness must be 0)
 # under_layers = "" # Empty string if no under layers
-under_layers = "10 1050 307 2258\n0 4000 2000 2500\n" # One substratum layer
+under_layers = "10 2000 1000 2000\n0 4500 2000 2500\n" # One sublayer + one substratum layer
 n_under_layers = under_layers.count('\n') # Number of under layers
 
 
@@ -153,9 +153,9 @@ trig  = 0 # data pretrig (if needed)
 
 
 # Frequency domain and sampling setup to compute dispersion
-nf = 46 # number of frequency samples [#]
+nf = 99 # number of frequency samples [#]
 df = 1 # frequency sample interval [Hz]
-min_f = 5 # minimum frequency [Hz]
+min_f = 1 # minimum frequency [Hz]
 max_f = min_f + (nf - 1) * df
 
 n_modes = 1 # Number of modes to compute
@@ -294,6 +294,20 @@ for iWT, WT in enumerate(WTs) :
     ### PLOTS ---------------------------------------------------------------------------------------------------------------------------------------
     color = color_map[iWT,:] # Color for the plot
 
+
+    zs_seismic = np.copy(zs)
+    if n_under_layers != 0:
+        for i, layer in enumerate(layers):
+            thickness, Vp, Vs, rho = np.fromstring(layer, dtype=float, sep=' ')
+            thickness_number = int(max(dz, thickness) // dz)
+            VSs = np.concatenate((VSs, [Vs]*thickness_number))
+            VPs = np.concatenate((VPs, [Vp]*thickness_number))
+            rhobs = np.concatenate((rhobs, [rho]*thickness_number))
+            
+            zs_to_add = np.linspace(zs_seismic[-1] - dz, zs_seismic[-1] - dz * thickness_number, thickness_number)                
+            zs_seismic = np.concatenate((zs_seismic, zs_to_add))
+            
+
     if iWT == 0:
         # Create figures
         figPage1, axs1 = plt.subplots(2, 3, figsize=(11.69,8.27), gridspec_kw={'hspace':0.3, 'wspace':0.4})
@@ -308,23 +322,23 @@ for iWT, WT in enumerate(WTs) :
 
 
     # Plot VPs vs. zs
-    axs1[0,0].plot(VPs, zs, linewidth=1.5, color=color)
+    axs1[0,0].plot(VPs, zs_seismic, linewidth=1.5, color=color)
     axs1[0,0].axhline(-WT, color=color, linestyle='--', linewidth=0.5)
-    axs1[0,0].set_xlim(0, 2000)
+    axs1[0,0].set_xlim(0, max(VPs) + 100)
     axs1[0,0].set_xlabel('$V_p$ [m/s]')
     axs1[0,0].set_ylabel('$z$ [m]')
 
     # Plot VSs vs. zs
-    axs1[0,1].plot(VSs, zs, linewidth=1.5, color=color)
+    axs1[0,1].plot(VSs, zs_seismic, linewidth=1.5, color=color)
     axs1[0,1].axhline(-WT, color=color, linestyle='--', linewidth=0.5)
-    axs1[0,1].set_xlim(0, 600)
+    axs1[0,1].set_xlim(0, max(VSs) + 100)
     axs1[0,1].set_xlabel('$V_s$ [m/s]')
     axs1[0,1].set_ylabel('$z$ [m]')
 
     # Plot rho_b vs. zs
-    axs1[0,2].plot(rhobs, zs, linewidth=1.5, color=color)
+    axs1[0,2].plot(rhobs, zs_seismic, linewidth=1.5, color=color)
     axs1[0,2].axhline(-WT, color=color, linestyle='--', linewidth=0.5)
-    axs1[0,2].set_xlim(1500, 2000)
+    axs1[0,2].set_xlim(min(rhobs) - 100, max(rhobs) + 100)
     axs1[0,2].set_xlabel('$\\rho_b$ [kg/$m^3$]')
     axs1[0,2].set_ylabel('$z$ [m]')
 
@@ -336,9 +350,10 @@ for iWT, WT in enumerate(WTs) :
     axs1[1,0].set_ylabel('$z$ [m]')
 
     # Plot Poisson ratio vs. zs
-    axs1[1,1].plot(list(map(fish, VPs, VSs)), zs, linewidth=1.5, color=color)
+    poisson_ratios = list(map(fish, VPs, VSs))
+    axs1[1,1].plot(poisson_ratios, zs_seismic, linewidth=1.5, color=color)
     axs1[1,1].axhline(-WT, color=color, linestyle='--', linewidth=0.5)
-    axs1[1,1].set_xlim(0, 0.5)
+    axs1[1,1].set_xlim(0, max(poisson_ratios) + 0.1)
     axs1[1,1].set_xlabel('Poisson ratio')
     axs1[1,1].set_ylabel('$z$ [m]')
 
@@ -348,37 +363,37 @@ for iWT, WT in enumerate(WTs) :
 
 
     # Plot VPs vs. zs
-    axs2[0,0].plot(VPs, zs, linewidth=1.5, color=color)
+    axs2[0,0].plot(VPs, zs_seismic, linewidth=1.5, color=color)
     axs2[0,0].axhline(-WT, color=color, linestyle='--', linewidth=0.5)
-    axs2[0,0].set_xlim(0, 2000)
+    axs2[0,0].set_xlim(0, max(VPs) + 100)
     axs2[0,0].set_xlabel('$V_p$ [m/s]')
     axs2[0,0].set_ylabel('$z$ [m]')
 
     # Plot VSs vs. zs
-    axs2[0,1].plot(VSs, zs, linewidth=1.5, color=color)
+    axs2[0,1].plot(VSs, zs_seismic, linewidth=1.5, color=color)
     axs2[0,1].axhline(-WT, color=color, linestyle='--', linewidth=0.5)
-    axs2[0,1].set_xlim(0, 600)
+    axs2[0,1].set_xlim(0, max(VSs) + 100)
     axs2[0,1].set_xlabel('$V_s$ [m/s]')
     axs2[0,1].set_ylabel('$z$ [m]')
 
     # Plot rho_b vs. zs
-    axs2[0,2].plot(rhobs, zs, linewidth=1.5, color=color)
+    axs2[0,2].plot(rhobs, zs_seismic, linewidth=1.5, color=color)
     axs2[0,2].axhline(-WT, color=color, linestyle='--', linewidth=0.5)
-    axs2[0,2].set_xlim(1500, 2000)
+    axs2[0,2].set_xlim(min(rhobs) - 100, max(rhobs) + 100)
     axs2[0,2].set_xlabel('$\\rho_b$ [kg/$m^3$]')
     axs2[0,2].set_ylabel('$z$ [m]')
 
     # Plot simulated P-wave first arrivals
     axs2[1,0].plot(xs, ThodPs, linewidth=1.5, color=color)
     axs2[1,0].set_xlim([0, max(xs)])
-    axs2[1,0].set_ylim([0, 0.25])
+    axs2[1,0].set_ylim([0, max(ThodPs)])
     axs2[1,0].set_xlabel('Offset [m]')
     axs2[1,0].set_ylabel('P- first arrival time [s]')
 
     # Plot simulated S-wave first arrivals
     axs2[1,1].plot(xs, ThodSs, linewidth=1.5, color=color)
     axs2[1,1].set_xlim([0, max(xs)])
-    axs2[1,1].set_ylim([0, 0.7])
+    axs2[1,1].set_ylim([0, max(ThodSs)])
     axs2[1,1].set_xlabel('Offset [m]')
     axs2[1,1].set_ylabel('S- first arrival time [s]')
 
@@ -393,7 +408,8 @@ for iWT, WT in enumerate(WTs) :
             min_vr = np.min(dispersion_data[mode][:,1])
         # np.savetxt(f'./output/{name}_M{mode}_WT{WT}.txt', dispersion_data[mode], fmt='%f') # Save dispersion data to file
     axs2[1,2].set_xlim([min_f-5, max_f+5])
-    axs2[1,2].set_ylim([min_vr-100, max_vr+100])
+    # axs2[1,2].set_ylim([min_vr-100, max_vr+100])
+    axs2[1,2].set_ylim([100, 500])
     axs2[1,2].set_xlabel('Frequency [Hz]')
     axs2[1,2].set_ylabel('P-SV phase vel. [m/s]')
     ### ---------------------------------------------------------------------------------------------------------------------------------------------
